fc5b70e68e60dfc9f083e470f9f4058fcf450aaa
Gio14 <67607867+Gio14@users.noreply.github.com>
2021-07-06 17:43:32 +0200

    First commit

---
+++ Gemini Prototype/Assets/Scripts/Rotation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+    public Transform target;
+    public float speed = 5f;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        Vector3 direction = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(direction);
+        transform.rotation = rotation;
+        
+    }
+}
---
+++ Gemini Prototype/Assets/Scripts/TwoPlayerInputs.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoPlayerInputs : MonoBehaviour
+{
+
+    //Player Objects
+    [SerializeField]
+    private GameObject Player1;
+    [SerializeField]
+    private GameObject Player2;
+    private float speed =0.04f;
+
+    private void Movement()
+    {
+        //Check if players are assigned
+        if (Player1 && Player2)
+        {
+            //Processing Player1 input for horizontal 
+            //This does the trick: Input.GetAxis("HorizontalPlayer1")
+            Player1.transform.position = new Vector3(Player1.transform.position.x + Input.GetAxis("HorizontalPlayer1") * speed, Player1.transform.position.y, Player1.transform.position.z);
+            //Processing Player1 input for vertical 
+            Player1.transform.position = new Vector3(Player1.transform.position.x, Player1.transform.position.y + Input.GetAxis("VerticalPlayer1") * speed, Player1.transform.position.z);
+
+            //Processing Player2 input for horizontal 
+            //This does the trick: Input.GetAxis("HorizontalPlayer2")
+            Player2.transform.position = new Vector3(Player2.transform.position.x + Input.GetAxis("HorizontalPlayer2")*speed, Player2.transform.position.y, Player2.transform.position.z);
+            //Processing Player2 input for vertical 
+            Player2.transform.position = new Vector3(Player2.transform.position.x, Player2.transform.position.y + Input.GetAxis("VerticalPlayer2")*speed, Player2.transform.position.z);
+        }
+    }
+    void Update()
+    {
+        Movement();
+    }
+}
