a48cd25fd039692df0a8b0966b58b3e326346282
Gio14 <67607867+Gio14@users.noreply.github.com>
2021-07-09 15:34:59 +0200

    Laser implementation

---
+++ Gemini Prototype/Assets/Scripts/Laser.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lr;
+    [SerializeField] private Transform startPoint;
+    [SerializeField] private Transform endPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+    
+    public void EnergyBeam()
+    {
+        if (Input.GetKey(KeyCode.E))
+        {
+            lr.enabled = true;
+        lr.SetPosition(0, startPoint.position);
+        lr.SetPosition(1, endPoint.position);
+        }
+        else
+        {
+            lr.enabled = false;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        EnergyBeam();
+    }
+
+            
+}
--- Gemini Prototype/Assets/Scripts/Rotation.cs
+++ Gemini Prototype/Assets/Scripts/Rotation.cs
@@ -7,18 +7,18 @@ public class Rotation : MonoBehaviour
     public Transform target;
     public float speed = 5f;
     // Start is called before the first frame update
-    private void Start()
+   
+    private void Alignment()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-     
         Vector3 direction = target.position - transform.position;
         Quaternion rotation = Quaternion.LookRotation(direction);
         transform.rotation = rotation;
         
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        Alignment();
     }
 }
