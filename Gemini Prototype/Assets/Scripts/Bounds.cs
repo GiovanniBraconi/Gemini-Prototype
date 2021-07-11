c60fa4ad42aaaba60a229b19ac200a59337b4d67
Gio14 <67607867+Gio14@users.noreply.github.com>
2021-07-11 15:21:02 +0200

    ScreenBounds added

---
+++ Gemini Prototype/Assets/Scripts/Bounds.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounds : MonoBehaviour
+{
+    private float xBound = 19f;
+    private float yBound = 9f;
+    private void ScreenBounds()
+    {
+        if (transform.position.x < -xBound)
+        {
+
+            transform.position = new Vector3(-xBound, transform.position.y, transform.position.z);
+
+
+        }
+        else if (transform.position.x > xBound)
+        {
+            transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
+
+        }
+        else if (transform.position.y > yBound)
+        {
+            transform.position = new Vector3(transform.position.x, yBound, transform.position.z);
+
+        }
+        else if (transform.position.y < -yBound)
+        {
+            transform.position = new Vector3(transform.position.x, -yBound, transform.position.z);
+
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+        {
+            ScreenBounds();
+        }
+    }
+
