0f41599eec765da93f42ffc6e02fe97e2fe217a9
Gio14 <67607867+Gio14@users.noreply.github.com>
2021-07-11 12:31:47 +0200

    Implemented the color change for each button pressed

--- Gemini Prototype/Assets/Scripts/Laser.cs
+++ Gemini Prototype/Assets/Scripts/Laser.cs
@@ -7,34 +7,84 @@ public class Laser : MonoBehaviour
     private LineRenderer lr;
     [SerializeField] private Transform startPoint;
     [SerializeField] private Transform endPoint;
+    
     // Start is called before the first frame update
     void Start()
     {
         lr = GetComponent<LineRenderer>();
     }
     
-    public void EnergyBeam()
+    public void BlueEnergyBeam()
     {
-        if (Input.GetKey(KeyCode.E))
-        {
+        
+            lr.startColor = Color.blue;
+            lr.endColor = Color.blue;
             lr.enabled = true;
         lr.SetPosition(0, startPoint.position);
         lr.SetPosition(1, endPoint.position);
+        
+
+       
+
+    }
+    public void RedEnergyBeam()
+    {
+       
+            lr.startColor = Color.red;
+            lr.endColor = Color.red;
+            lr.enabled = true;
+            lr.SetPosition(0, startPoint.position);
+            lr.SetPosition(1, endPoint.position);
+        
+       
+
+
+    }
+    public void MagentaEnergyBeam()
+    {
+        
+            lr.startColor = Color.magenta;
+            lr.endColor = Color.magenta;
+            lr.enabled = true;
+            lr.SetPosition(0, startPoint.position);
+            lr.SetPosition(1, endPoint.position);
+        
+        
+
+
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+        if (Input.GetKey(KeyCode.E))
+        {
+        BlueEnergyBeam();
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            RedEnergyBeam();
+        }
+        else if (Input.GetKey(KeyCode.C))
+        {
+            MagentaEnergyBeam();
         }
         else
         {
             lr.enabled = false;
         }
-
-
+        
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+}
+       
+        
 
-        EnergyBeam();
-    }
+        
+
 
             
-}
