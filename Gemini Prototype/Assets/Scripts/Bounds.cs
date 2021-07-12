996a0da6da5db2150b8b394fd8e3e123e09e2aea
Gio14 <67607867+Gio14@users.noreply.github.com>
2021-07-12 12:22:38 +0200

    ScreenBounds method fixed

--- Gemini Prototype/Assets/Scripts/Bounds.cs
+++ Gemini Prototype/Assets/Scripts/Bounds.cs
@@ -6,6 +6,10 @@ public class Bounds : MonoBehaviour
 {
     private float xBound = 19f;
     private float yBound = 9f;
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+
+    
     private void ScreenBounds()
     {
         if (transform.position.x < -xBound)
@@ -15,21 +19,26 @@ public class Bounds : MonoBehaviour
 
 
         }
-        else if (transform.position.x > xBound)
+        if  (transform.position.x > xBound)
         {
             transform.position = new Vector3(xBound, transform.position.y, transform.position.z);
 
         }
-        else if (transform.position.y > yBound)
+        if  (transform.position.y > yBound)
         {
             transform.position = new Vector3(transform.position.x, yBound, transform.position.z);
 
         }
-        else if (transform.position.y < -yBound)
+        if  (transform.position.y < -yBound)
         {
             transform.position = new Vector3(transform.position.x, -yBound, transform.position.z);
 
         }
+    }
+    private void Bond()
+    {
+        
+        player1.transform.position = player1.transform.position + (player2.transform.position - player1.transform.position).normalized * 20;
     }
 
         // Update is called once per frame
@@ -37,5 +46,6 @@ public class Bounds : MonoBehaviour
         {
             ScreenBounds();
         }
-    }
+    
+}
 
